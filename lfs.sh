54851a639e66227b08045a143ebdeb8b2b209c72
CMAgent007 <CMAgent007@gmail.com>
2025-05-27 15:21:42 +0530

    file on progress

--- lfs.sh
+++ lfs.sh
@@ -9,3 +9,18 @@ if | grep -q  "$LFS" /proc/mounts; then
     sudo mount "${LFS_DISK}2" "$LFS"
     sudo chown -v $USER "$LFS"
 fi
+
+mkdir -pv $LFS/sources
+mkdir -pv $LFS/tools
+
+mkdir -pv $LFS/boot
+mkdir -pv $LFS/etc
+mkdir -pv $LFS/bin
+mkdir -pv $LFS/lib
+mkdir -pv $LFS/sbin
+mkdir -pv $LFS/usr
+mkdir -pv $LFS/var
+
+case $(uname -m) in
+   x86_64) mkdir -pv $LFS/lib64 ;;
+esac
